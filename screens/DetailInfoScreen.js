6894fc5ba6ad0d1386caa406d25254c9cb773cff
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-08-11 21:28:43 +0300

    Minor refactoring

--- screens/DetailInfoScreen.js
+++ screens/DetailInfoScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Alert, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import { BarChart, XAxis } from 'react-native-svg-charts';
+import { LineChart, Path, Grid } from 'react-native-svg-charts'
 import 'react-native-svg';
 import { detailInfoRequest } from '../app/actions/fetching_actions';
 
@@ -21,12 +21,25 @@ class DetailInfoScreen extends Component {
   render() {
     const { detailCityInfo } = this.props;
     let tempList = [];
+
     if (detailCityInfo != null) {
-      for (var i = 0; i < detailCityInfo.list.length; i++){
+      for (var i = 0; i < detailCityInfo.list.length; i++) {
         tempList.push(detailCityInfo.list[i].main.temp);
       }
       console.log(tempList, "TEMP LIST");
     }
+
+    const Shadow = ({ line }) => (
+      <Path
+        key={'shadow'}
+        y={2}
+        d={line}
+        fill={'none'}
+        strokeWidth={4}
+        stroke={'rgba(134, 65, 244, 0.2)'}
+      />
+    )
+
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.container}>
@@ -35,16 +48,41 @@ class DetailInfoScreen extends Component {
               <View style={styles.textInfo}>
                 <Text>{detailCityInfo.city.name}</Text>
               </View>
+            )
+            : null
+          }
+          {detailCityInfo
+            ? (
               <View style={styles.textInfo}>
                 <Text>{detailCityInfo.city.country}</Text>
               </View>
+            )
+            : null
+          }
+          {detailCityInfo
+            ? (
               <View style={styles.textInfo}>
                 <Text>{detailCityInfo.city.population}</Text>
               </View>
             )
             : null
           }
-        
+          {detailCityInfo
+            ? (
+              <View style={{ height: 200 }}>
+                <LineChart
+                  style={{ height: 200 }}
+                  data={tempList}
+                  svg={{ stroke: 'rgb(134, 65, 244)' }}
+                  contentInset={{ top: 20, bottom: 20 }}
+                >
+                  <Grid />
+                  <Shadow />
+                </LineChart>
+              </View>
+            )
+            : null
+          }
         </View>
       </SafeAreaView>
     );
