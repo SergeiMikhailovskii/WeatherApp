b1e1584abb8fc5ad82f15dee14c9b9bd29e4aea5
siarhei.mikhaylouski <35fulofe>
2019-08-15 11:35:18 +0300

    Added icons to bottom tabs

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function TabBarIcon(props) {
   return (
     <View>
-      <Text>p</Text>
+
     </View>
   );
 }
