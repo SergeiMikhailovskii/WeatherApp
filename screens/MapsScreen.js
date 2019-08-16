801c0654421cec5d0c8595755f480c49d12fee97
siarhei.mikhaylouski <35fulofe>
2019-08-16 12:28:23 +0300

    Added markers

--- screens/MapsScreen.js
+++ screens/MapsScreen.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
-export default function MapsScreen() {
-  return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: 53.8878349,
-        longitude: 27.5428332,
-        latitudeDelta: 0.0122,
-        longitudeDelta: 0.0121,
-      }}
-    />
-  );
+export default class MapsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [{
+        title: 'Title 1',
+        coordinates: {
+          latitude: 53.8978349,
+          longitude: 27.5428332
+        },
+      },
+      {
+        title: 'Title 2',
+        coordinates: {
+          latitude: 53.8878349,
+          longitude: 27.5428332
+        },
+      }]
+    };
+  }
+
+  render() {
+    const { markers } = this.state;
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 53.8878349,
+          longitude: 27.5428332,
+          latitudeDelta: 0.0122,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        {markers.map(marker => (
+          <MapView.Marker
+            coordinate={marker.coordinates}
+            title={marker.title}
+          />
+        ))}
+      </MapView>
+    );
+  }
 }
 
 MapsScreen.navigationOptions = {
